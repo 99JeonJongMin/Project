c666f40c9576b7ba4ad1a8c8eda146a12f1186c6
전종민 <jnjjb05@naver.com>
2025-03-13 09:49:25 +0900

    config

--- 2024Project/src/main/java/cs/dit/config/MyBatisConfig.java
+++ 2024Project/src/main/java/cs/dit/config/MyBatisConfig.java
@@ -6,6 +6,7 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -15,23 +16,24 @@ import javax.sql.DataSource;
 @MapperScan("cs.dit.mapper") // MyBatis 매퍼 스캔
 @ComponentScan(basePackages = {"cs.dit.domain", "cs.dit.service", "cs.dit.controller"}) // 기존 XML의 context:component-scan 대체
 public class MyBatisConfig {
+	
+	 	@Value("${spring.datasource.url}")
+	    private String url;
 
+	    @Value("${spring.datasource.username}")
+	    private String username;
+
+	    @Value("${spring.datasource.password}")
+	    private String password;
+	    
 	@Bean
 	public DataSource dataSource() {
-	    HikariConfig hikariConfig = new HikariConfig();
-	    hikariConfig.setDriverClassName("com.mysql.cj.jdbc.Driver");
-	    // 환경 변수에서 직접 읽기
-	    String host = System.getenv("MYSQL_HOST");
-	    String port = System.getenv("MYSQL_PORT");
-	    String database = System.getenv("MYSQL_DATABASE");
-	    String username = System.getenv("MYSQL_USER");
-	    String password = System.getenv("MYSQL_PASSWORD");
-
-	    hikariConfig.setJdbcUrl("jdbc:mysql://" + host + ":" + port + "/" + database);
-	    hikariConfig.setUsername(username);
-	    hikariConfig.setPassword(password);
-
-	    return new HikariDataSource(hikariConfig);
+		  HikariConfig config = new HikariConfig();
+	        config.setJdbcUrl(url);
+	        config.setUsername(username);
+	        config.setPassword(password);
+	        config.setDriverClassName("com.mysql.cj.jdbc.Driver");
+	        return new HikariDataSource(config);
 	}
 
     @Bean
