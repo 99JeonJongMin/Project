1a0982df6596363e5d94c43345648d566b7f3737
전종민 <jnjjb05@naver.com>
2025-03-14 12:01:38 +0900

    idk

--- 2024Project/src/main/java/cs/dit/config/MyBatisConfig.java
+++ 2024Project/src/main/java/cs/dit/config/MyBatisConfig.java
@@ -5,7 +5,7 @@ import com.zaxxer.hikari.HikariDataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
-import org.mybatis.spring.annotation.MapperScan;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -13,7 +13,6 @@ import org.springframework.core.env.Environment;
 import javax.sql.DataSource;
 
 @Configuration
-@MapperScan("cs.dit.mapper") // MyBatis 매퍼 스캔
 @ComponentScan(basePackages = {"cs.dit.domain", "cs.dit.service", "cs.dit.controller"}) // 기존 XML의 context:component-scan 대체
 public class MyBatisConfig {
     
@@ -41,7 +40,7 @@ public class MyBatisConfig {
         System.out.println("MYSQLDATABASE: " + database);
         System.out.println("MYSQLUSER: " + username);
         System.out.println("MYSQLPASSWORD: " + password);
-
+ 
         // JDBC URL 설정
         String jdbcUrl = String.format("jdbc:mysql://%s:%s/%s?serverTimezone=UTC&characterEncoding=UTF-8", host, port, database);
         
--- 2024Project/src/main/java/cs/dit/mapper/MemberMapper.java
+++ 2024Project/src/main/java/cs/dit/mapper/MemberMapper.java
@@ -3,7 +3,7 @@ package cs.dit.mapper;
 import org.apache.ibatis.annotations.*;
 import cs.dit.domain.MemberVO;
 
-
+@Mapper
 public interface MemberMapper {
 
     @Insert("INSERT INTO member (userid, passwd, name, email) VALUES (#{userid}, #{passwd}, #{name}, #{email})")
@@ -12,4 +12,3 @@ public interface MemberMapper {
     @Select("SELECT userid, passwd FROM member WHERE userid = #{userid} AND passwd = #{passwd}")
     MemberVO login(MemberVO member); // ✅ MemberVO 객체를 직접 받도록 수정
 }
- 
