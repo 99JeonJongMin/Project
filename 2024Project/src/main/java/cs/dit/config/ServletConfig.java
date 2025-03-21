b847dec0975a1ba33f04babf901664e117bed3d3
전종민 <jnjjb05@naver.com>
2025-03-21 18:22:02 +0900

    lalalalalalst

--- 2024Project/src/main/java/cs/dit/config/ServletConfig.java
+++ 2024Project/src/main/java/cs/dit/config/ServletConfig.java
@@ -1,5 +1,6 @@
 package cs.dit.config;
 
+import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletPath;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,5 +15,10 @@ public class ServletConfig {
         ServletRegistrationBean<DispatcherServlet> registrationBean = new ServletRegistrationBean<>(servlet, "/");
         registrationBean.setLoadOnStartup(1);  // 서버 시작 시 로드
         return registrationBean;
+    }
+    
+    @Bean
+    public DispatcherServletPath dispatcherServletPath() {
+        return () -> "/";
     }
 }
