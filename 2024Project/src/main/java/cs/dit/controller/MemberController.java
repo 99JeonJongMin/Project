8a974d9cdad9d1e9b44a1b75b63ed3db97feb737
전종민 <jnjjb05@naver.com>
2025-03-21 17:17:21 +0900

    두걸음씩

--- 2024Project/src/main/java/cs/dit/controller/MemberController.java
+++ 2024Project/src/main/java/cs/dit/controller/MemberController.java
@@ -5,7 +5,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import cs.dit.domain.MemberVO;
