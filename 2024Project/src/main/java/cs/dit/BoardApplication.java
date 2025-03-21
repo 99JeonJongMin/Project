8b05eab3ed5beb41d9b908336db78fbba25c443f
전종민 <jnjjb05@naver.com>
2025-03-21 18:50:04 +0900

    ㅈㅈ

--- 2024Project/src/main/java/cs/dit/BoardApplication.java
+++ 2024Project/src/main/java/cs/dit/BoardApplication.java
@@ -6,13 +6,11 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class}) // 🔥 Spring Security 기본 설정 제거
 @ServletComponentScan  // ✅ 서블릿, 필터, 리스너 자동 감지
-@ComponentScan(basePackages = {"cs.dit.controller", "cs.dit.service", "cs.dit.mapper", "cs.dit.config", "cs.dit.domain"})  // ✅ 컨트롤러 강제 스캔
 public class BoardApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
