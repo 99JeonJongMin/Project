d8bdbd53cc08dd4070d800e36300e90aa992899a
전종민 <jnjjb05@naver.com>
2025-03-24 15:08:58 +0900

    test

--- 2024Project/src/main/java/cs/dit/config/SecurityConfig.java
+++ 2024Project/src/main/java/cs/dit/config/SecurityConfig.java
@@ -13,13 +13,33 @@ public class SecurityConfig {
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 	    http
 	        .authorizeHttpRequests(auth -> auth
-	            .anyRequest().permitAll()
+	        		.requestMatchers(
+	        			    "/", 
+	        			    "/board/login", 
+	        			    "/board/index", 
+	        			    "/board/logout", 
+	        			    "/board/**", 
+	        			    "/resources/**", 
+	        			    "/test/open"
+	        			).permitAll()
+	            .anyRequest().authenticated()
 	        )
-	        .csrf(csrf -> csrf.disable());
+	        .csrf(csrf -> csrf.disable())
+	        .formLogin(login -> login
+	            .loginPage("/board/login")
+	            .defaultSuccessUrl("/board/index", true)
+	            .permitAll()
+	        )
+	        .logout(logout -> logout
+	            .logoutUrl("/board/logout")
+	            .logoutSuccessUrl("/board/index")
+	            .permitAll()
+	        );
+	    System.out.println("✅ Security filter chain 설정됨");
+
 
 	    return http.build();
 	}
-
 
     // 🔐 비밀번호 암호화를 위한 BCrypt 인코더 Bean 등록
     @Bean
